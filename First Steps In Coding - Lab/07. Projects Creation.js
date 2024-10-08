14e9c17c1b49bde3efbcc1fbd54fc46f4b853806
Ivan Kourtev <135639014+IvanKourtev@users.noreply.github.com>
2024-10-08 11:47:32 +0300

    Added 7th task

---
+++ First Steps In Coding - Lab/07. Projects Creation.js
@@ -0,0 +1,6 @@
+function hoursNeeded(name, projects){
+    let hours = projects * 3;
+    console.log(`The architect ${name} will need ${hours} hours to complete ${projects} project/s.`);
+}
+
+hoursNeeded("Ivan", 4)
