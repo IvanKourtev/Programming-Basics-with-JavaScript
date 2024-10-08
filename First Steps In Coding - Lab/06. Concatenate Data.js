85ec16bd7ab95cccf000d369f17194344d974bf7
Ivan Kourtev <135639014+IvanKourtev@users.noreply.github.com>
2024-10-08 11:47:23 +0300

    Added 6th task

---
+++ First Steps In Coding - Lab/06. Concatenate Data.js
@@ -0,0 +1,5 @@
+function makeSentence(firstName, lastName, age, town){
+    console.log(`You are ${firstName} ${lastName}, a ${age}-years old person from ${town}.`);
+}
+
+makeSentence("Ivan", "Kourtev", 20, "Sofia");
